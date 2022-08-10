809973e668492e5c17512a55e852181cfa31f372
tulsi1104 <tulsiii1104@gmail.com>
2022-08-10 14:34:40 +0530

    practical14 completed

---
+++ src/main/kotlin/practical4.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+
+    println("Enter a number")
+    var x= readLine()!! .toInt()
+    if (x %2==0)
+        println("Entered a number $x is even")
+    else
+        println("Entered a number $x is odd")
+
+
+}
