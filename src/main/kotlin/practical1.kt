81a4aa6ac3b538c1f33a249e16788b284b7aa5f3
MADpractical1_21012022046 <ozadutt562002@gmail.com>
2022-08-23 01:56:40 -0700

    complete practical 1

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,4 +1,10 @@
-fun main()
-{
-    println("hellow world")
+fun main() {
+    var x = 1352
+    val y:Double = 55.5
+    val z:Long = 56200221
+    val a: Char ='D'
+    println("integer value=$x")
+    println("Double value=$y")
+    println("long value=$z")
+    println("char value=$a")
 }
