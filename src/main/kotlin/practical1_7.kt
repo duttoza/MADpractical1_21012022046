0c87d1528496e3519b3ab06ef5e41dfa97213c5f
MADpractical1_21012022046 <ozadutt562002@gmail.com>
2022-08-23 21:49:49 -0700

    complate

---
+++ src/main/kotlin/practical1_7.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    println("Enter number:")
+    var number: Int = readLine()!!.toInt()
+    val factorial = fact(number)
+    println("Factorial of $number = $factorial")
+    println("by tailrec keyword,factorial of $number is $number=$factorial")
+}
+tailrec fun fact(n: Int, temp: Int = 1): Int {
+    return if (n == 1){
+        temp
+    } else {
+        fact(n-1, temp*n)
+    }
+}
