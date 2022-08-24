c3edb67dd2148688ebd113b6b9381c0b5f8d90b0
MADpractical1_21012022046 <ozadutt562002@gmail.com>
2022-08-23 22:05:06 -0700

    complate pr1_10

---
+++ src/main/kotlin/practical1_10.kt
@@ -0,0 +1,31 @@
+class Car(type:String, model:String, price:Int, owner:String, miles:Int){
+    var typee:String = type
+    var modell =model
+    var pricee:Int = price
+    var ownerr = owner
+    var miless:Int = miles
+    fun getCarInfo() {
+        println("Selling Price of Car: " + (pricee - miless).toString() + "rs")
+        println("Actual price: " + pricee.toString() + "rs")
+        println("Total Kms: $miless")
+        println("car model:$modell")
+        println("car owner: $ownerr")
+        println("car miles: $miless")
+        println()
+
+    }
+    fun getOriginalCarPrice() {
+        println("Actual price: " + pricee.toString() + "rs")
+        println()
+
+    }
+}
+fun main() {
+    var c1 = Car("SUV", "FORTUNER", 130000, "dutt", 10000)
+    c1.getCarInfo()
+    var c2 = Car(" SUV", "NEXON", 100000, "pushpak", 50000)
+    c2.getCarInfo()
+    println("*************************")
+    c1.getOriginalCarPrice()
+    c2.getOriginalCarPrice()
+}
